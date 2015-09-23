61bf2923fe8cba837511f8e82da100baca18f22d
torcato <cesar.matos@gmail.com>
2015-09-23 10:34:56 +0200

    Adding 3d Graphs

--- static/js/raw_draw.js
+++ static/js/raw_draw.js
@@ -12,92 +12,106 @@ var draw_data = {
 //data structure with functions to visualize the data
 var draw_functions  = {
     table : function(){
-		    draw_data.last_draw = "table";
-		    //graph_div.empty();
-		    var t = dataToTable(draw_data.data);
-		    var visualization = new google.visualization.Table(graph_div);
-		    visualization.draw(t);
-	    },
+	    draw_data.last_draw = "table";
+	    //graph_div.empty();
+	    var t = dataToTable(draw_data.data);
+	    var visualization = new google.visualization.Table(graph_div);
+	    visualization.draw(t);
+    },
     columnChart : function(){ 
-		          draw_data.last_draw = "columnChart";
-		          var t = dataToTable(draw_data.data);
-		          var visualization = new google.visualization.ColumnChart(graph_div);
-		          visualization.draw(t);
-	          },
+        draw_data.last_draw = "columnChart";
+        var t = dataToTable(draw_data.data);
+        var visualization = new google.visualization.ColumnChart(graph_div);
+        visualization.draw(t);
+    },
     barChart : function(){ 
-		       draw_data.last_draw = "barChart";
-		       var t = dataToTable(draw_data.data);
-		       var visualization = new google.visualization.BarChart(graph_div);
-		       visualization.draw(t);
-	       },    
+        draw_data.last_draw = "barChart";
+        var t = dataToTable(draw_data.data);
+        var visualization = new google.visualization.BarChart(graph_div);
+        visualization.draw(t);
+    },    
     pieChart : function(){ 
-		       draw_data.last_draw = "pieChart"
-		       var t = dataToTable(draw_data.data);
-		       var visualization = new google.visualization.PieChart(graph_div);
-		       visualization.draw(t);
-	       },
+        draw_data.last_draw = "pieChart"
+        var t = dataToTable(draw_data.data);
+        var visualization = new google.visualization.PieChart(graph_div);
+        visualization.draw(t);
+    },
     scatterChart : function(){
-		           draw_data.last_draw = "scatterChart"
-                   var t = dataToTable(draw_data.data);
-		           var visualization = new google.visualization.ScatterChart(graph_div);
-		           visualization.draw(t);
-	           },
+        draw_data.last_draw = "scatterChart"
+        var t = dataToTable(draw_data.data);
+        var visualization = new google.visualization.ScatterChart(graph_div);
+        visualization.draw(t);
+    },
     areaChart : function (){ 
-		        draw_data.last_draw = "areaChart";
-                var t = dataToTable(draw_data.data);
-		        var visualization = new google.visualization.AreaChart(graph_div);
-		        visualization.draw(t);
-	        },
+        draw_data.last_draw = "areaChart";
+        var t = dataToTable(draw_data.data);
+        var visualization = new google.visualization.AreaChart(graph_div);
+        visualization.draw(t);
+    },
     histogram: function(){
-		       draw_data.last_draw = "histogram";
-               var t = dataToTable(draw_data.data);
-		       var chart = new google.visualization.Histogram(graph_div);
-		       chart.draw(t);
-	       },
+        draw_data.last_draw = "histogram";
+        var t = dataToTable(draw_data.data);
+        var chart = new google.visualization.Histogram(graph_div);
+        chart.draw(t);
+    },
     line_chart: function(){
-		        draw_data.last_draw = "line_chart";
-		        var chart = new google.visualization.LineChart(graph_div);
-		        var t = dataToTable(draw_data.data);
-		        chart.draw(t);
-	        },
+        draw_data.last_draw = "line_chart";
+        var chart = new google.visualization.LineChart(graph_div);
+        var t = dataToTable(draw_data.data);
+        chart.draw(t);
+    },
     geo_world: function(){
-                // this is because google maps screws the css
-                // by inserting a div just for drawing at least does not screw up other graphs
-                graph_div.innerHTML = '<div id="map_canvas" style="height: 100%; width: 100%;"></div>';
-                draw_data.last_draw = "geo_world";
-                var chart = new google.visualization.Map(document.getElementById('map_canvas'));
-                options = {
-                    mapType : 'normal',
-                    showTip : true
-                }
-                var t = dataToTable(draw_data.data);
-                chart.draw(t, options);
-	       },
+        // this is because google maps screws the css
+        // by inserting a div just for drawing at least does not screw up other graphs
+        graph_div.innerHTML = '<div id="map_canvas" style="height: 100%; width: 100%;"></div>';
+        draw_data.last_draw = "geo_world";
+        var chart = new google.visualization.Map(document.getElementById('map_canvas'));
+        options = {
+            mapType : 'normal',
+            showTip : true
+        }
+        var t = dataToTable(draw_data.data);
+        chart.draw(t, options);
+    },
     sunburts: function(){
-                draw_data.last_draw = "sunburts";
-                d = dataToHierarchy(draw_data.data);
-                draw_sunburst(d, graph_div);
-	       },
+        draw_data.last_draw = "sunburts";
+        d = dataToHierarchy(draw_data.data);
+        draw_sunburst(d, graph_div);
+    },
     tree: function(){
-                draw_data.last_draw = "tree";
-                d = dataToHierarchy(draw_data.data);
-                draw_tree(d, graph_div);
-	       },
+        draw_data.last_draw = "tree";
+        d = dataToHierarchy(draw_data.data);
+        draw_tree(d, graph_div);
+    },
     circle_pack: function(){
-                draw_data.last_draw = "circle_pack";
-                d = dataToHierarchy(draw_data.data);
-                draw_circle_packing(d, graph_div);
-	       },
+        draw_data.last_draw = "circle_pack";
+        d = dataToHierarchy(draw_data.data);
+        draw_circle_packing(d, graph_div);
+    },
     treemap: function(){
-                draw_data.last_draw = "treemap";
-                d = dataToHierarchy(draw_data.data);
-                draw_treemap(d, graph_div);
-	       },
+        draw_data.last_draw = "treemap";
+        d = dataToHierarchy(draw_data.data);
+        draw_treemap(d, graph_div);
+    },
     bubble_chart: function(){
-                draw_data.last_draw = "bubble_chart";
-                d = dataToHierarchy(draw_data.data);
-                draw_bubblechart(d, graph_div);
-	       },
+        draw_data.last_draw = "bubble_chart";
+        d = dataToHierarchy(draw_data.data);
+        draw_bubblechart(d, graph_div);
+    },
+    graph_3d : function(){
+        draw_data.last_draw = "graph_3d";
+        var t = dataToTable(draw_data.data);
+        var options = { 
+            style: "bar",
+            width: ""+graph_div.offsetWidth -10+ "px",
+            height: ""+graph_div.offsetHeight -10+ "px"
+        };
+        var chart = new links.Graph3d(graph_div);
+        chart.draw(t, options);
+    },
+    image: function(){
+        
+    }
 }
 
 // There is a bug with when drawing tables, so if the tab is not selected it takes the full height, 
@@ -142,3 +156,5 @@ document.getElementById('draw_tree').onclick =  draw_functions['tree'];
 document.getElementById('draw_circle_pack').onclick =  draw_functions['circle_pack'];
 document.getElementById('draw_treemap').onclick =  draw_functions['treemap'];
 document.getElementById('draw_bubblechart').onclick =  draw_functions['bubble_chart'];
+
+document.getElementById('draw_3d').onclick =  draw_functions['graph_3d'];
