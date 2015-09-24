bd0b180c1458222d50b9258a55d37ae0a12f5581
torcato <cesar.matos@gmail.com>
2015-09-24 09:56:21 +0200

    developing heatmap usin d3.js

--- static/js/draw_functions.js
+++ static/js/draw_functions.js
@@ -1,4 +1,101 @@
-// Function to draw a sunberts sequence as seen in 
+
+//Function to draw a 2d heatmap matrix as seen in
+//http://bl.ocks.org/mbostock/3074470
+function draw_heatmap(heatmap, div){
+
+  var width = div.offsetWidth ,
+    height = div.offsetHeight ;
+
+  var dx = heatmap[0].length,
+      dy = heatmap.length;
+
+  // Fix the aspect ratio.
+  // var ka = dy / dx, kb = height / width;
+  // if (ka < kb) height = width * ka;
+  // else width = height / ka;
+
+  var x = d3.scale.linear()
+      .domain([0, dx])
+      .range([0, width]);
+
+  var y = d3.scale.linear()
+      .domain([0, dy])
+      .range([height, 0]);
+
+/*  var color = d3.scale.linear()
+      .domain([95, 115, 135, 155, 175, 195])
+      .range(["#0a0", "#6c0", "#ee0", "#eb4", "#eb9", "#fff"]);
+*/
+
+    var vmax= heatmap.reduce( function(max, arr){
+      return Math.max( max,  Math.max.apply( Math, arr ))
+    }, -Infinity);
+
+    var vmin = heatmap.reduce( function(max, arr){
+      return Math.min( max,  Math.min.apply( Math, arr ))
+    }, +Infinity);
+
+  console.log("max/min", vmax, vmin);
+  var color = d3.scale.linear()
+      .domain([vmin, vmax])
+      .range(["#fff" ,"#004"]);
+
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("top")
+      .ticks(20);
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("right");
+
+  d3.select("body").append("canvas")
+      .attr("width", dx)
+      .attr("height", dy)
+      .style("width", width + "px")
+      .style("height", height + "px")
+      .call(drawImage);
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .call(removeZero);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .call(removeZero);
+
+  // Compute the pixel colors; scaled by CSS.
+  function drawImage(canvas) {
+    var context = canvas.node().getContext("2d"),
+        image = context.createImageData(dx, dy);
+
+    for (var y = 0, p = -1; y < dy; ++y) {
+      for (var x = 0; x < dx; ++x) {
+        var c = d3.rgb(color(heatmap[y][x]));
+        image.data[++p] = c.r;
+        image.data[++p] = c.g;
+        image.data[++p] = c.b;
+        image.data[++p] = 255;
+      }
+    }
+
+    context.putImageData(image, 0, 0);
+  }
+
+  function removeZero(axis) {
+    axis.selectAll("g").filter(function(d) { return !d; }).remove();
+  }
+}
+
+// Function to draw a sunburst sequence as seen in 
 //http://bl.ocks.org/kerryrodden/7090426
 function draw_sunburst(data, div) {
     console.log("drawing sunburst");
