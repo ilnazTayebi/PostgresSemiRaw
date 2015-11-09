5c6eb22db8ea9400ca409b25e115081853eb18a5
torcato <cesar.matos@gmail.com>
2015-11-09 15:38:14 +0100

    changed the docker way of starting

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ 
+docker_ip=$(ip addr | awk '/inet/ && /docker0/{sub(/\/.*$/,"",$2); print $2}')
+sed -e  "s/__host_ip__/$docker_ip/g" conf/nginx_template.conf > conf/nginx.conf
+
+docker run -d -p 5000:5000 \
+    -v $PWD:/usr/share/nginx/html:ro \
+    -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
+    -v $PWD/log:/var/log/nginx:rw \
+    nginx 
