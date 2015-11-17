c99a1f4a300f038652a0c62fa07255e0a468379e
Nuno Santos <nuno@raw-labs.com>
2015-11-17 13:38:08 +0100

    Use port 80 instead of 5000. Moved location of web site to top level (remove /static)

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 docker_ip=$(ip addr | awk '/inet/ && /docker0/{sub(/\/.*$/,"",$2); print $2}')
 sed -e  "s/__executor_ip__/$docker_ip/g" conf/nginx_template.conf > conf/nginx.conf
 
-docker run -d -p 5000:5000 \
-    -v $PWD:/usr/share/nginx/html:ro \
+docker run -d -p 80:80 \
+    -v $PWD/static:/usr/share/nginx/html:ro \
     -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
     -v $PWD/log:/var/log/nginx:rw \
     nginx 
