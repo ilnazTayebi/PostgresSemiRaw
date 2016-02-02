515bc7c3f76f5d40847bb2e089ebe2d5dfa9f614
Nuno Santos <nuno@raw-labs.com>
2016-02-02 11:18:20 +0100

    Create a dockerhost entry in /etc/hosts of the container pointing to the host (--add-host docker option)

--- simple_auth.sh
+++ simple_auth.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
  
 docker_ip=$(ip addr | awk '/inet/ && /docker0/{sub(/\/.*$/,"",$2); print $2}')
-sed -e  "s/__executor_ip__/$docker_ip/g" conf/nginx_auth_template.conf > conf/nginx.conf
 
 docker run -d -p 5000:5000 \
     -v $PWD:/usr/share/nginx/html:ro \
-    -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
+    -v $PWD/conf/nginx_auth.conf:/etc/nginx/nginx.conf:ro \
     -v $PWD/conf/.htpasswd:/etc/nginx/.htpasswd:ro \
     -v $PWD/log:/var/log/nginx:rw \
+    --add-host dockerhost:${docker_ip} \
     nginx 
--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
  
 docker_ip=$(ip addr | awk '/inet/ && /docker0/{sub(/\/.*$/,"",$2); print $2}')
-sed -e  "s/__executor_ip__/$docker_ip/g" conf/nginx_template.conf > conf/nginx.conf
+echo "Found docker host ip: $docker_ip"
 
 docker run -d -p 80:80 \
     -v $PWD/static:/usr/share/nginx/html:ro \
     -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
     -v $PWD/log:/var/log/nginx:rw \
+    --add-host dockerhost:${docker_ip} \
     nginx 
