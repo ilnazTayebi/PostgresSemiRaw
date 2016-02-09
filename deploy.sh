9ca55d6c874ed344fabaed5d3adeb4ad3893b249
Nuno Santos <nuno@raw-labs.com>
2016-02-09 13:53:07 +0100

    Add deploy script to upload static web pages to S3

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ FULLPATH=/tmp/$FILENAME
 
 echo Writing to $FULLPATH
 tar zcf $FULLPATH static/ rest-client/
+
 DESTKEY=s3://raw-deploy/$FILENAME
+LATESTKEY=s3://raw-deploy/frontend-latest.tgz
 echo Uploading to $DESTKEY
 aws s3 cp $FULLPATH $DESTKEY
-aws s3 cp --acl public-read  $DESTKEY s3://raw-deploy/frontend-latest.tgz
+echo Copying to $DESTKEY $LATESTKEY
+aws s3 cp --acl public-read  $DESTKEY $LATESTKEY
