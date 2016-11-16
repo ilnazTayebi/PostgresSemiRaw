2cd2c137b23ad8a61b9bf75b30d2d900e71a942f
cesar matos <cesar.matos@gmail.com>
2016-11-16 15:15:54 +0100

    Upgraded ace editor version

---
+++ static/js/ace/snippets/qrawl.js
@@ -0,0 +1,5 @@
+ace.define("ace/snippets/sql",["require","exports","module"],function(e,t,n){
+    "use strict";
+    t.snippetText="snippet s*\n	select * from ${1:source}\n",
+    t.scope="sql"
+})
---
+++ static/js/ace/snippets/rawschema.js
@@ -0,0 +1,5 @@
+ace.define("ace/snippets/sql",["require","exports","module"],function(e,t,n){
+    "use strict";
+    t.snippetText="collection()",
+    t.scope="sql"
+})
