a86997af7b4b5be7945b156ad16909bd484e1efb
ilnazTayebi <72614875+ilnazTayebi@users.noreply.github.com>
2024-10-31 00:14:26 +0100

    Added Sniffer

--- postgresRaw/postgresql-9.6.5/src/backend/snooping/auxiliary/common.c
+++ postgresRaw/postgresql-9.6.5/src/backend/snooping/auxiliary/common.c
@@ -1,12 +1,12 @@
 /*
-                        NoDB Project 
-        Query Processing On Raw Data Files using PostgresRAW
+						NoDB Project
+		Query Processing On Raw Data Files using PostgresRAW
 
-                   Copyright (c) 2011-2013
+				   Copyright (c) 2011-2013
   Data Intensive Applications and Systems Labaratory (DIAS)
-           Ecole Polytechnique Federale de Lausanne
+		   Ecole Polytechnique Federale de Lausanne
 
-                     All Rights Reserved.
+					 All Rights Reserved.
 
 Permission to use, copy, modify and distribute this software and its
 documentation is hereby granted, provided that both the copyright notice
@@ -28,13 +28,10 @@ USE OF THIS SOFTWARE.
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
-
+#include <dirent.h>
 
 #include "snooping/common.h"
-//#include "snooping/metadata.h"
-
-
-
+// #include "snooping/metadata.h"
 
 /* From pg_ctl.c */
 static char **readfile(const char *path);
@@ -44,31 +41,33 @@ static void *pg_malloc(size_t size);
 static int parse(char *buf, char **args);
 
 void initialiazeConfiguration(void);
-//void printConfiguration(void);
+void printConfiguration(void);
 int checkConfiguration(void);
 int searchSlotInConfiguration(int value);
 
-
 /*Global variables*/
-//static char *build = NULL;
+// static char *build = NULL;
 char *configuration_filename = "snoop.conf";
+/* TODO: impliment more generic way for datasetdir */
+char *datasetdir = "datasets";
 
-//Load information from snoop.conf file
-//Mapping data-file, relation, delimiter
+// Load information from snoop.conf file
+// Mapping data-file, relation, delimiter
 ConfigurationFile CF;
-
-
 
 char *getInputFilename(char *relation)
 {
 	int i;
-	if(CF.loaded != true)
+	if (CF.loaded != true)
 		return NULL;
-	for ( i = 0; i < NUMBER_OF_RELATIONS; i++){
-		if( CF.files[i].id != -1)
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
+	{
+		if (CF.files[i].id != -1)
 		{
-			if(strcmp(CF.files[i].relation, relation) == 0)
+			if (strcmp(CF.files[i].relation, relation) == 0)
+			{
 				return CF.files[i].filename;
+			}
 		}
 	}
 	return NULL;
@@ -77,12 +76,13 @@ char *getInputFilename(char *relation)
 char *getDelimiter(char *relation)
 {
 	int i;
-	if(CF.loaded != true)
+	if (CF.loaded != true)
 		return NULL;
-	for ( i = 0; i < NUMBER_OF_RELATIONS; i++){
-		if( CF.files[i].id != -1)
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
+	{
+		if (CF.files[i].id != -1)
 		{
-			if(strcmp(CF.files[i].relation, relation) == 0)
+			if (strcmp(CF.files[i].relation, relation) == 0)
 				return CF.files[i].delimiter;
 		}
 	}
@@ -92,12 +92,13 @@ char *getDelimiter(char *relation)
 bool getHeader(char *relation)
 {
 	int i;
-	if(CF.loaded != true)
+	if (CF.loaded != true)
 		return false;
-	for ( i = 0; i < NUMBER_OF_RELATIONS; i++){
-		if( CF.files[i].id != -1)
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
+	{
+		if (CF.files[i].id != -1)
 		{
-			if(strcmp(CF.files[i].relation, relation) == 0)
+			if (strcmp(CF.files[i].relation, relation) == 0)
 				return CF.files[i].header;
 		}
 	}
@@ -121,27 +122,36 @@ bool isLoaded(void)
 bool configurationExists(void)
 {
 	FILE *infile;
+	DIR *indir;
 	char buf[MAX_PATH_LENGTH];
 	char cwd[MAX_PATH_LENGTH];
+	char datasetPath[MAX_PATH_LENGTH];
 
 	getcwd(cwd, MAX_PATH_LENGTH);
 	Assert(cwd != NULL);
 
 	snprintf(buf, MAX_PATH_LENGTH, "%s/%s", cwd, configuration_filename);
 
-	if ((infile = fopen(buf, "r")) == NULL){
-//		fprintf(stderr, "File %s not found...",buf);
+	if ((infile = fopen(buf, "r")) == NULL)
+	{
+		//		fprintf(stderr, "File %s not found...",buf);
 		return false;
 	}
 	fclose(infile);
+	snprintf(datasetPath, MAX_PATH_LENGTH, "%s/../../%s", cwd, datasetdir);
+	if ((indir = opendir(datasetPath)) == NULL)
+	{
+		fprintf(stderr, "Dataset directory not fount. Directory %s not found...", datasetPath);
+		return false;
+	}
+	closedir(indir);
 	return true;
 }
 
-void
-initialiazeConfiguration(void)
+void initialiazeConfiguration(void)
 {
 	int i = 0;
-	for ( i = 0; i < NUMBER_OF_RELATIONS; i++)
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
 	{
 		strcpy(CF.files[i].filename, "\0");
 		strcpy(CF.files[i].relation, "\0");
@@ -152,53 +162,50 @@ initialiazeConfiguration(void)
 	CF.loaded = false;
 }
 
-void
-printConfiguration(void)
+void printConfiguration(void)
 {
 	int i = 0;
 
-	fprintf(stdout,"\nPrint configuration information:\n");
-	fprintf(stdout,"Configuration filename: %s\n", CF.conf_file);
-	fprintf(stdout,"PostgreSQL data folder : %s\n", CF.pg_data);
-	fprintf(stdout,"PostgreSQL path : %s\n", CF.path);
-//	fprintf(stdout,"Loaded : %d\n", CF.loaded);
-	fprintf(stdout,"Available pointers to files: %d\n", CF.how_many);
-	for ( i = 0; i < NUMBER_OF_RELATIONS; i++)
+	fprintf(stdout, "\nPrint configuration information:\n");
+	fprintf(stdout, "Configuration filename: %s\n", CF.conf_file);
+	fprintf(stdout, "PostgreSQL data folder : %s\n", CF.pg_data);
+	fprintf(stdout, "PostgreSQL path : %s\n", CF.path);
+	//	fprintf(stdout,"Loaded : %d\n", CF.loaded);
+	fprintf(stdout, "Available pointers to files: %d\n", CF.how_many);
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
 	{
-		if( CF.files[i].id != -1)
+		if (CF.files[i].id != -1)
 		{
-//			fprintf(stdout,"ID : {%d} Pos = {%d}\n", CF.files[i].id, i);
-			fprintf(stdout,"%d) File: %s\n",(i+1), CF.files[i].filename);
-			fprintf(stdout,"Relation: %s\n", CF.files[i].relation);
-			fprintf(stdout,"Delimiter : %s\n", CF.files[i].delimiter);
-			fprintf(stdout,"Header : %s\n", CF.files[i].header ? "True" : "False");
+			//			fprintf(stdout,"ID : {%d} Pos = {%d}\n", CF.files[i].id, i);
+			fprintf(stdout, "\n");
+			fprintf(stdout, "%d) File: %s\n", (i + 1), CF.files[i].filename);
+			fprintf(stdout, "Relation: %s\n", CF.files[i].relation);
+			fprintf(stdout, "Delimiter : %s\n", CF.files[i].delimiter);
+			fprintf(stdout, "Header : %s\n\n", CF.files[i].header ? "True" : "False");
 			fflush(stdout);
 		}
 	}
-	fprintf(stdout,"\n\n");
+	fprintf(stdout, "\n\n");
 }
 
-int
-checkConfiguration(void)
+int checkConfiguration(void)
 {
 	int i = 0;
-	for ( i = 0; i < NUMBER_OF_RELATIONS; i++)
-		if( CF.files[i].id != -1)
-			if ( strlen(CF.files[i].filename) == 0 || strlen(CF.files[i].relation) == 0 || strlen(CF.files[i].delimiter) == 0)
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
+		if (CF.files[i].id != -1)
+			if (strlen(CF.files[i].filename) == 0 || strlen(CF.files[i].relation) == 0 || strlen(CF.files[i].delimiter) == 0)
 				return -1;
 	return 1;
 }
 
-
-int
-searchSlotInConfiguration(int value)
+int searchSlotInConfiguration(int value)
 {
 	int i = 0;
-	for ( i = 0; i < NUMBER_OF_RELATIONS; i++)
-		if( CF.files[i].id == value)
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
+		if (CF.files[i].id == value)
 			return i;
-	for ( i = 0; i < NUMBER_OF_RELATIONS; i++)
-		if( CF.files[i].id == -1)
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
+		if (CF.files[i].id == -1)
 		{
 			CF.how_many++;
 			return i;
@@ -206,29 +213,26 @@ searchSlotInConfiguration(int value)
 	return -1;
 }
 
-bool
-isInConfigFile(char *relation)
+bool isInConfigFile(char *relation)
 {
-	int i = 0 ;
-	for( i = 0; i < NUMBER_OF_RELATIONS; i++)
+	int i = 0;
+	for (i = 0; i < NUMBER_OF_RELATIONS; i++)
 	{
 		if (CF.files[i].id != -1)
 		{
-			if( strcmp (CF.files[i].relation, relation) == 0)
+			if (strcmp(CF.files[i].relation, relation) == 0)
 				return true;
 		}
 	}
 	return false;
 }
 
-
 /*
  * Load environmental parameters for SnoopDB filename: CF.path/CF.pg_data/configuration_filename
  *
  * Parameters: input filename, relation, delimiter, header
  */
-void
-loadEnvironment(void)
+void loadEnvironment(void)
 {
 	char **result;
 	char buf[MAX_PATH_LENGTH];
@@ -240,112 +244,117 @@ loadEnvironment(void)
 	size = strlen(CF.pg_data);
 
 	getcwd(buf, MAX_PATH_LENGTH);
-	CF.path = (char*) pg_malloc ( MAX_PATH_LENGTH * sizeof(char));
+	CF.path = (char *)pg_malloc(MAX_PATH_LENGTH * sizeof(char));
 	strcpy(CF.path, buf);
 	CF.path[strlen(CF.path) - size] = '\0';
 
 	Assert(CF.pg_data != NULL);
 
-	//TODO: change how I locate the snoop.conf file... (need for more general solution)
+	// TODO: change how I locate the snoop.conf file... (need for more general solution)
 	if (CF.pg_data)
 	{
 		CF.pg_data = xstrdup(CF.pg_data);
-//		snprintf(conf_file, MAX_PATH_LENGTH, "%s/snoop.conf", pg_data);
-		//Read configuration file not from data but from PostreSQL directory
-//		snprintf(CF.conf_file, MAX_PATH_LENGTH, "%s/snoop.conf", CF.path);
-		snprintf(CF.conf_file, MAX_PATH_LENGTH, "%s/%s/%s", CF.path,CF.pg_data,configuration_filename);
+		//		snprintf(conf_file, MAX_PATH_LENGTH, "%s/snoop.conf", pg_data);
+		// Read configuration file not from data but from PostreSQL directory
+		//		snprintf(CF.conf_file, MAX_PATH_LENGTH, "%s/snoop.conf", CF.path);
+		snprintf(CF.conf_file, MAX_PATH_LENGTH, "%s/%s/%s", CF.path, CF.pg_data, configuration_filename);
 	}
 
 	result = readfile(CF.conf_file);
 	if (result != NULL)
 		for (; *result != NULL; result++)
-			if(strlen(*result) > 1)	//Consider "\n"
+			if (strlen(*result) > 1) // Consider "\n"
 			{
-				//Up to 16 tokens per line in the configuration file
+				// Up to 16 tokens per line in the configuration file
 				char *args[16];
-				if ( parse(*result, args) == 3)
+
+				if (parse(*result, args) == 3)
 				{
 					int size = strlen(args[2]);
-					if(strncmp(args[0], "filename-", 9) == 0 && strcmp(args[1], "=") == 0)
+					if (strncmp(args[0], "filename-", 9) == 0 && strcmp(args[1], "=") == 0)
 					{
 						int val = atoi(args[0] + 9);
 						int pos = searchSlotInConfiguration(val);
-						if( pos != -1)
+						if (pos != -1)
 						{
 							strncpy(CF.files[pos].filename, args[2] + 1, size - 1);
 							CF.files[pos].filename[size - 2] = '\0';
 							CF.files[pos].id = val;
 						}
 					}
-					else if(strncmp(args[0], "relation-" ,9) == 0 && strcmp(args[1], "=") == 0)
+					else if (strncmp(args[0], "relation-", 9) == 0 && strcmp(args[1], "=") == 0)
 					{
 						int val = atoi(args[0] + 9);
 						int pos = searchSlotInConfiguration(val);
 						int tmp;
-						if( pos != -1)
+						if (pos != -1)
 						{
 							strncpy(CF.files[pos].relation, args[2] + 1, size - 1);
 							CF.files[pos].relation[size - 2] = '\0';
-							for ( tmp = 0; tmp < strlen(CF.files[pos].relation); tmp++)
+							for (tmp = 0; tmp < strlen(CF.files[pos].relation); tmp++)
 								CF.files[pos].relation[tmp] = tolower(CF.files[pos].relation[tmp]);
 							CF.files[pos].id = val;
 						}
 					}
-					else if(strncmp(args[0], "delimiter-", 10) == 0 && strcmp(args[1], "=") == 0)
+					else if (strncmp(args[0], "delimiter-", 10) == 0 && strcmp(args[1], "=") == 0)
 					{
+
 						int val = atoi(args[0] + 10);
 						int pos = searchSlotInConfiguration(val);
-						if( pos != -1)
+						if (pos != -1)
 						{
 							strncpy(CF.files[pos].delimiter, args[2] + 1, size - 1);
+
 							CF.files[pos].delimiter[size - 2] = '\0';
+
 							CF.files[pos].id = val;
 						}
 					}
-					else if(strncmp(args[0], "header-", 7) == 0 && strcmp(args[1], "=") == 0)
+					else if (strncmp(args[0], "header-", 7) == 0 && strcmp(args[1], "=") == 0)
 					{
 						int val = atoi(args[0] + 7);
 						int pos = searchSlotInConfiguration(val);
-						if( pos != -1)
+						if (pos != -1)
 						{
-							char arg2[size-1];
+							char arg2[size - 1];
 							strncpy(arg2, args[2] + 1, size - 1);
 							arg2[size - 2] = '\0';
-							if (strcmp( arg2, "True") == 0){
+							if (strcmp(arg2, "True") == 0)
+							{
 								CF.files[pos].header = true;
 							}
-							else {
+							else
+							{
 								CF.files[pos].header = false;
 							}
 							CF.files[pos].id = val;
 						}
 					}
-//					else if(strcmp(args[0], "build") == 0 && strcmp(args[1], "=") == 0)
-//					{
-//						build = (char*) pg_malloc ( (size - 1) * sizeof(char));
-//						strncpy(build, args[2] + 1, size - 1);
-//						build[size - 2] = '\0';
-//					}
+					//					else if(strcmp(args[0], "build") == 0 && strcmp(args[1], "=") == 0)
+					//					{
+					//						build = (char*) pg_malloc ( (size - 1) * sizeof(char));
+					//						strncpy(build, args[2] + 1, size - 1);
+					//						build[size - 2] = '\0';
+					//					}
 				}
 			}
 	CF.loaded = true;
 
-	Assert( CF.how_many < NUMBER_OF_RELATIONS);
-	Assert( checkConfiguration() != -1);
+	Assert(CF.how_many < NUMBER_OF_RELATIONS);
+	Assert(checkConfiguration() != -1);
 
-	//printConfiguration();
+	printConfiguration();
 }
-
 
 static char *
 xstrdup(const char *s)
 {
-	char	   *result;
+	char *result;
 
 	result = strdup(s);
 	if (!result)
 	{
-//		write_stderr(_("%s: out of memory\n"), progname);
+		//		write_stderr(_("%s: out of memory\n"), progname);
 		exit(1);
 	}
 	return result;
@@ -354,7 +363,7 @@ xstrdup(const char *s)
 static void *
 pg_malloc(size_t size)
 {
-	void	   *result;
+	void *result;
 
 	result = malloc(size);
 	if (!result)
@@ -370,16 +379,17 @@ pg_malloc(size_t size)
 static char **
 readfile(const char *path)
 {
-	FILE	   *infile;
-	int			maxlength = 1,
-				linelen = 0;
-	int			nlines = 0;
-	char	  **result;
-	char	   *buffer;
-	int			c;
+	FILE *infile;
+	int maxlength = 1,
+		linelen = 0;
+	int nlines = 0;
+	char **result;
+	char *buffer;
+	int c;
 
-	if ((infile = fopen(path, "r")) == NULL){
-		fprintf(stderr, "File %s not found...",path);
+	if ((infile = fopen(path, "r")) == NULL)
+	{
+		fprintf(stderr, "File %s not found...", path);
 		return NULL;
 	}
 	/* pass over the file twice - the first time to size the result */
@@ -403,8 +413,8 @@ readfile(const char *path)
 		maxlength = linelen;
 
 	/* set up the result and the line buffer */
-	result = (char **) pg_malloc((nlines + 1) * sizeof(char *));
-	buffer = (char *) pg_malloc(maxlength + 1);
+	result = (char **)pg_malloc((nlines + 1) * sizeof(char *));
+	buffer = (char *)pg_malloc(maxlength + 1);
 
 	/* now reprocess the file and store the lines */
 	rewind(infile);
@@ -419,10 +429,7 @@ readfile(const char *path)
 	return result;
 }
 
-
-
-int
-parse(char *buf, char **args)
+int parse(char *buf, char **args)
 {
 	int counter = 0;
 	while (*buf != '\0')
@@ -439,16 +446,92 @@ parse(char *buf, char **args)
 	return counter;
 }
 
-int
-getFirstFreeFromMap(int *map, int size)
+int getFirstFreeFromMap(int *map, int size)
 {
 	int i = 0;
 	for (i = 0; i < size; i++)
 	{
-		if(map[i] == -1)
+		if (map[i] == -1)
 			return i;
 	}
 	return -1;
 }
 
+/* SemiRaw:
+ * Function to get the relation name from the file name
+ */
+char getRelationName(char *filename)
+{
+	char *pos = strstr(filename, ".csv");
 
+	if (pos != NULL)
+	{
+		*pos = '\0';
+	}
+}
+
+/*  SemiRaw:
+ * Write to snoop.conf
+ */
+void writeConfig()
+{
+	/* TODO: make the relation to the lowercase to not have .scv or .CSV */
+	char datasetPath[MAX_PATH_LENGTH];
+	char conf_file[MAX_FILENAME];
+	char filePath[MAX_PATH_LENGTH];
+	char *pg_data;
+	char relation[MAX_RELATION_NAME];
+	char delimiter = ',';
+	struct dirent *content;
+	DIR *dir;
+	FILE *conffile;
+
+	pg_data = getenv("PGDATA");
+	snprintf(conf_file, MAX_PATH_LENGTH, "%s/%s", pg_data, configuration_filename);
+
+	if ((conffile = fopen(conf_file, "w")) == NULL)
+	{
+		fprintf(stderr, "File %s not found...", conffile);
+		return false;
+	}
+
+	snprintf(datasetPath, MAX_PATH_LENGTH, "%s/../../%s", pg_data, datasetdir);
+
+	if ((dir = opendir(datasetPath)) == NULL)
+	{
+		fprintf(stderr, "Dataset directory %s not found...", datasetPath);
+		return;
+	}
+
+	int fileIndex = 0;
+
+	while ((content = readdir(dir)) != NULL)
+	{
+		if (content->d_name[0] == '.' && (content->d_name[1] == '\0' || (content->d_name[1] == '.' && content->d_name[2] == '\0')))
+			continue;
+
+		/* For now, only consider .csv files */
+		if (strstr(content->d_name, ".csv") != NULL)
+		{
+			fileIndex++;
+
+			snprintf(filePath, MAX_PATH_LENGTH, "%s/%s", datasetPath, content->d_name);
+
+			strncpy(relation, content->d_name, MAX_RELATION_NAME - 1);
+			relation[MAX_RELATION_NAME - 1] = '\0';
+			getRelationName(relation);
+			if (fileIndex != 1)
+				fprintf(conffile, "\n", fileIndex, filePath);
+			fprintf(conffile, "filename-%d = '%s'\n", fileIndex, filePath);
+			fprintf(conffile, "relation-%d = '%s'\n", fileIndex, relation);
+			fprintf(conffile, "delimiter-%d = '%c'\n", fileIndex, delimiter);
+			fprintf(conffile, "header-%d = 'True'\n", fileIndex);
+		}
+	}
+	// fprintf(conffile, "\n", fileIndex, filePath);
+
+	fclose(conffile);
+	closedir(dir);
+
+	fprintf("Configuration written to %s\n", configuration_filename);
+}
